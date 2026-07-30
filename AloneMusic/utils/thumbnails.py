eb03c6360888fd502fd39dd281ae38af7fc0bda8
Sɪɢᴍᴀ Tʜᴀᴋᴜʀ 👀 <reetsingh3666@gmail.com>
2026-07-31 02:33:32 +0530

    Update and rename thumbnails (1).py to thumbnails.py

--- AloneMusic/utils/thumbnails.py
+++ AloneMusic/utils/thumbnails.py
@@ -16,7 +16,7 @@ import os, aiofiles, aiohttp
 from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont
 from py_yt import VideosSearch
 from config import YOUTUBE_IMG_URL
-from ShashankMusic import app
+from AloneMusic import app
 
 CACHE_DIR = "cache"
 os.makedirs(CACHE_DIR, exist_ok=True)
